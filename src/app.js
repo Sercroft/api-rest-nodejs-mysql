2a04a2e598202cea948ac44bb4683bb018533912
Andrus <fugitivesebas31@hotmail.es>
2023-12-05 16:55:00 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -6,14 +6,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors({ origin: '*'}));
 app.use('/api', UsersRoutes);
 app.use(IndexRoutes);
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     res.status(404).json({
         message: 'Endpoint not found!'
     });
